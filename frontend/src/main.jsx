7a1d499dfc174f5ddb93c8c0612f56d5ba746e32
Ndc203 <chuanghost203@gmail.com>
2025-10-29 16:26:13 +0700

    trang hien thi danh sach san phẩm

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,12 +7,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // Import các trang
 import AdminCategoriesPage from "./pages/admin/AdminCategoriesPage.jsx";
 import AdminPostPage from "./pages/admin/AdminPostPage.jsx";
+import AdminProductsPage from "./pages/admin/AdminProductsPage.jsx";
 import Login from "./pages/auth/Login.jsx";
 import Register from "./pages/auth/Register.jsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <AdminCategoriesPage /> },
   { path: "/admin/categories", element: <AdminCategoriesPage /> },
+  { path: "/admin/products", element: <AdminProductsPage /> },
   { path: "/admin/posts", element: <AdminPostPage /> },
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
